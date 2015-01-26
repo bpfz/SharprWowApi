8418d10e516a1d91516a9321492b33821b1e1e7d
bpfz <Robineriks@gmail.com>
2015-01-26 08:25:19 +0100

    timestamp int to long

--- SharprWowApi/Models/Character/CharacterProgressionBoss.cs
+++ SharprWowApi/Models/Character/CharacterProgressionBoss.cs
@@ -6,12 +6,12 @@ namespace SharprWowApi.Models.Character
         public int Id { get; set; }
         public string Name { get; set; }
         public int NormalKills { get; set; }
-        public object NormalTimestamp { get; set; }
+        public long NormalTimestamp { get; set; }
         public int? HeroicKills { get; set; }
         public long? HeroicTimestamp { get; set; }
         public int? LfrKills { get; set; }
         public long? LfrTimestamp { get; set; }
         public int? MythicKills { get; set; }
-        public int? MythicTimestamp { get; set; }
+        public long? MythicTimestamp { get; set; }
     }
 }
