d21d12bd10bf0422d03a668be65e007e57b652e5
bpfz <Robineriks@gmail.com>
2015-01-27 18:49:03 +0100

    .

--- SharprWowApi/Enums.cs
+++ SharprWowApi/Enums.cs
@@ -36,30 +36,30 @@ namespace SharprWowApi
         zh_CN
     }
 
-    [Flags]
     public enum CharacterOptions
     {
-        None = 0,
-        GetGuild = 1,
-        GetStats = 2,
-        GetTalents = 4,
-        GetItems = 8,
-        GetReputation = 16,
-        GetTitles = 32,
-        GetProfessions = 64,
-        GetAppearance = 128,
-        GetPetSlots = 256,
-        GetMounts = 512,
-        GetPets = 1024,
-        GetAchievements = 2048,
-        GetProgression = 4096,
-        GetFeed = 8192,
-        GetPvP = 16384,
-        GetQuests = 32768,
-        GetHunterPets = 65536,
+        None,
+        GetGuild,
+        GetStats,
+        GetTalents,
+        GetItems,
+        GetReputation,
+        GetTitles,
+        GetProfessions,
+        GetAppearance,
+        GetPetSlots,
+        GetMounts,
+        GetPets,
+        GetAchievements,
+        GetProgression,
+        GetFeed,
+        GetPvP,
+        GetQuests,
+        GetHunterPets,
         GetEverything = GetGuild | GetStats | GetTalents | GetItems | GetReputation | GetTitles
         | GetProfessions | GetAppearance | GetPetSlots | GetMounts | GetPets
         | GetAchievements | GetProgression | GetFeed | GetPvP | GetQuests | GetHunterPets
+
     }
 
     [Flags]
