9b6440602a924f4d787960ea57814d3b640ead96
bpfz <Robineriks@gmail.com>
2015-01-27 17:54:51 +0100

    updated to represent changes done to the api

--- SharprWowApi/Models/Auction/Auction.cs
+++ SharprWowApi/Models/Auction/Auction.cs
@@ -1,18 +1,24 @@
 ﻿
+using System.Collections.Generic;
 namespace SharprWowApi.Models.Auction
 {
     public class Auction
     {
         public int Auc { get; set; }
-        public long Item { get; set; }
+        public int Item { get; set; }
         public string Owner { get; set; }
         public string OwnerRealm { get; set; }
-        public long Bid { get; set; }
-        public long Buyout { get; set; }
+        public int Bid { get; set; }
+        public int Buyout { get; set; }
         public int Quantity { get; set; }
         public string TimeLeft { get; set; }
-        public long Rand { get; set; }
-        public long Seed { get; set; }
-        public long Context { get; set; }
+        public int Rand { get; set; }
+        public int Seed { get; set; }
+        public int Context { get; set; }
+        public List<Modifier> Modifiers { get; set; }
+        public int? PetSpeciesId { get; set; }
+        public int? PetBreedId { get; set; }
+        public int? PetLevel { get; set; }
+        public int? PetQualityId { get; set; }
     }
 }
---
+++ SharprWowApi/Models/Auction/Modifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharprWowApi.Models.Auction
+{
+    public class Modifier
+    {
+        public int type { get; set; }
+        public int value { get; set; }
+    }
+}
