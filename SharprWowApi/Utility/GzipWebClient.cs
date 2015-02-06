c4de31a30757e89470e8c012c57ba1bd7c558809
bpfz <Robineriks@gmail.com>
2015-02-06 16:17:50 +0100

    Class derive from webclient override  Getwebrequest to enable automatic decrompression of gzip in webclient.

---
+++ SharprWowApi/Utility/GzipWebClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace SharprWowApi.Utility
+{
+    class GzipWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address) as HttpWebRequest;
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            return request;
+        }
+    }
+}
