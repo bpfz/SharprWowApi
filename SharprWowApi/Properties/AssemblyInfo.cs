f80b82b71624b68e26aa9e5ee7ddc459b439a07a
bpfz <Robineriks@gmail.com>
2015-02-12 04:20:29 +0100

    1.1

--- SharprWowApi/Properties/AssemblyInfo.cs
+++ SharprWowApi/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SharprWowApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescriptionAttribute("World of warcraft API wrapper Library. Written in C# .Net 4.5")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompanyAttribute("bpfz")]
 [assembly: AssemblyProduct("SharprWowApi")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyrightAttribute("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
