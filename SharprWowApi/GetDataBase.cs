c612a22fd7899d9ffafe8056676ba3293d10bc12
bpfz <Robineriks@gmail.com>
2015-02-12 04:19:16 +0100

    base class for GetData and GetDataAsync

---
+++ SharprWowApi/GetDataBase.cs
@@ -0,0 +1,18 @@
+﻿using SharprWowApi.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharprWowApi
+{
+    public abstract class GetDataBase
+    {
+        internal Region _Region { get; set; }
+        internal Locale _Locale { get; set; }
+        internal string _APIKey { get; set; }
+        internal string _Host { get; set; }
+        internal string _Realm { get; set; }
+    }
+}
