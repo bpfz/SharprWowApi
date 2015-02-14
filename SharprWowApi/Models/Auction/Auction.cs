22e3bcafdb95c4c599a8eaf9f521a079c3845f72
bpfz <Robineriks@gmail.com>
2015-02-14 08:04:41 +0100

    added bid & buyout gold silver copper (get them  separately)

--- SharprWowApi/Models/Auction/Auction.cs
+++ SharprWowApi/Models/Auction/Auction.cs
@@ -1,5 +1,7 @@
 ﻿
+using System;
 using System.Collections.Generic;
+using SharprWowApi.Utility.ExtensionMethods;
 namespace SharprWowApi.Models.Auction
 {
     public class Auction
@@ -8,8 +10,17 @@ namespace SharprWowApi.Models.Auction
         public int Item { get; set; }
         public string Owner { get; set; }
         public string OwnerRealm { get; set; }
-        public long Bid { get; set; }
-        public long Buyout { get; set; }
+
+        /// <summary>
+        /// gets bid gets buyout (gold+silver+bronze) as one number
+        /// </summary>
+        public string Bid { get; set; }
+
+        /// <summary>
+        /// gets buyout (gold+silver+bronze) as one number
+        /// </summary>
+        public string Buyout { get; set; }
+
         public int Quantity { get; set; }
         public string TimeLeft { get; set; }
         public int Rand { get; set; }
@@ -21,5 +32,13 @@ namespace SharprWowApi.Models.Auction
         public int? PetBreedId { get; set; }
         public int? PetLevel { get; set; }
         public int? PetQualityId { get; set; }
+
+        public string BidGold { get { return Bid.RemoveTail(4); } }
+        public string BidSilver { get { return Bid.Tail(4).RemoveTail(2); } }
+        public string BidCopper { get { return Bid.Tail(2); } }
+
+        public string BuyoutGold { get { return Buyout.RemoveTail(4); } }
+        public string BuyoutSilver { get { return Buyout.Tail(4).RemoveTail(2); } }
+        public string BuyoutCopper { get { return Buyout.Tail(2); } }
     }
 }
