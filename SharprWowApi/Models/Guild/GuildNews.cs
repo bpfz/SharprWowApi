23270da4a4d169e363d795642105e750d6e4bc35
bpfz <Robineriks@gmail.com>
2015-02-16 15:51:11 +0100

    added datetimeStamp that gets formatted timestamp

--- SharprWowApi/Models/Guild/GuildNews.cs
+++ SharprWowApi/Models/Guild/GuildNews.cs
@@ -1,12 +1,24 @@
 ﻿
+using System;
+using SharprWowApi.Utility.ExtensionMethods;
+
 namespace SharprWowApi.Models.Guild
 {
     public class GuildNews
     {
         public string Type { get; set; }
         public string Character { get; set; }
+
+        /// <summary>
+        /// Unix timestamp (long number)
+        /// </summary>
         public long Timestamp { get; set; }
         public int ItemId { get; set; }
         public GuildAchievement Achievement { get; set; }
+
+        /// <summary>
+        /// Timestamp datetime format. 
+        /// </summary>
+        public DateTime DateTimestamp { get { return Timestamp.UnixLongTimeStampToDateTime(); } }
     }
 }
