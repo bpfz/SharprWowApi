dbfd2415fcb373319215cf8068f2022a4aa571d2
bpfz <Robineriks@gmail.com>
2015-02-24 09:49:07 +0100

    Removed unused code.

--- SharprWowApi/GetDataBase.cs
+++ SharprWowApi/GetDataBase.cs
@@ -18,25 +18,5 @@ namespace SharprWowApi
         internal string _Host { get; set; }
 
         internal string _Realm { get; set; }
-
-        internal string UrlStringBuilder(string param, string name, CharacterOptions characterOptions, string realm)
-        {
-            this._Realm = realm;
-
-            var uriBuilder = new UriBuilder();
-            uriBuilder.Host = this._Host;
-            uriBuilder.Path = string.Format(
-                @"{0}/wow/character/{1}/{2}?locale={3}{4}&apikey={5}",
-                this._Host,
-                realm,
-                name,
-                this._Locale,
-                CharacterUtility.BuildOptionalFields(characterOptions),
-                this._APIKey);
-
-            uriBuilder.Scheme = "Https";
-
-            return uriBuilder.ToString();
-        }
     }
 }
