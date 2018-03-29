7e7199673bc77400a2e87fef40a2b7a732ca3671
Robin Eriksson Selin <Robin.ErikssonSelin@coreit.se>
2018-03-29 20:55:17 +0200

    removed non-async code changed JsonSerialize.Deserialize to JsonConvert.Deserialize

--- SharprWowApi/Utility/JsonUtility.cs
+++ SharprWowApi/Utility/JsonUtility.cs
@@ -10,59 +10,6 @@ namespace SharprWowApi.Utility
 {
     public class JsonUtility
     {
-        internal T GetDataFromURL<T>(string url) where T : class
-        {
-            try
-            {
-                return this.DeserializeJson<T>(url);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        private string DownloadString(string url)
-        {
-            using (var webClient = new GzipWebClient())
-            {
-                webClient.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
-                var downloadedString = webClient.DownloadString(url);
-
-                return downloadedString;
-            }
-        }
-
-        /// <summary>
-        /// Takes a jsonUtility string, downloads it with webclient, 
-        /// initializes memorystream, read stream w streamreader
-        /// returns deserialized object.
-        /// </summary>
-        /// <typeparam name="T">Class model</typeparam>
-        /// <param name="url">Url for the jsonUtility</param>
-        /// <returns>Deserialized object
-        /// </returns>
-        private T DeserializeJson<T>(string url) where T : class
-        {
-            try
-            {
-                var downloadedString = this.DownloadString(url);
-
-                using (var memoryStream = new MemoryStream(Encoding.Default.GetBytes(downloadedString)))
-                {
-                    var sr = new StreamReader(memoryStream);
-                    var jsonTextReader = new JsonTextReader(sr);
-
-                    var serializer = new JsonSerializer();
-                    return serializer.Deserialize<T>(jsonTextReader);
-                }
-            }
-            catch (ArgumentNullException)
-            {
-                throw;
-            }
-        }
-
         #region async
         internal async Task<T> GetDataFromURLAsync<T>(string url) where T : class
         {
@@ -102,17 +49,16 @@ namespace SharprWowApi.Utility
         {
             try
             {
-                var downloadedByte = await this.DownloadBytesAsync(url);
+                var downloadedByte = await DownloadBytesAsync(url);
 
                 using (var memoryStream = new MemoryStream(downloadedByte, false))
-                using (var sr = new StreamReader(memoryStream))
-                using (var jsonTextReader = new JsonTextReader(sr))
                 {
-                    var serializer = new JsonSerializer();
-                    var deserialize = new Task<T>(() => serializer.Deserialize<T>(jsonTextReader));
-                    deserialize.Start();
-                    var apiResponseObject = await deserialize;
-                    return apiResponseObject;
+                    using (var sr = new StreamReader(memoryStream))
+                    {
+                        var content = await sr.ReadToEndAsync();
+                        var deserialized = JsonConvert.DeserializeObject<T>(content);
+                        return deserialized;
+                    }
                 }
             }
             catch (ArgumentNullException)
